a214b0bd5d85caa3c0e5b97751a99188433f5021
Quinn Krug <quinn.krug@gmail.com>
2020-03-05 16:29:11 -0700

    set up champ toggle button

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import Champ from './Champ/Champ';
 
 class App extends Component {
 
+  //STATE
   state = {
     champs: [
       {name: 'Zed', enemytips: 'Zed receives bonus effect from Attack Damage items he purchases, so Armor is super effective against him. After Zed uses his Living Shadow, he is at his most vulnerable as his damage, slow, and mobility are both dependent upon it. Zeds Shadow Slash can only slow you if his shadow hits you with it.'},
       {  name: 'Lux', enemytips: 'Lux has powerful zone control abilities. Try to spread out and attack from different directions so she cannot lock down a specific area. When retreating with low health, be prepared to dodge Luxs Final Spark, a red targeting beam fires prior to the main beam, so try to move to the side if possible.'},
       { name: 'Ahri', enemytips: 'Ahris survivability is dramatically reduced when her Ultimate, Spirit Rush, is down. Stay behind minions to make Charm difficult to land, this will reduce Ahris damage potential significantly'}
-    ]
+    ],
+    showChamps: false
   }
 
+//FUNCTIONS (METHODS)
+toggleChampsHandler = () => {
+    const doesShow = this.state.showChamps;
+    this.setState( { showChamps: !doesShow } );
+  }
+
+// BEGIN RENDERING
   render() {
     return (
       <div className="App">
@@ -20,16 +29,15 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <button className="Champ-button" onClick={this.toggleChampsHandler} > Toggle Champs</button>
         <p className="App-intro">
-        {this.state.champs.map( ( champ, index ) => {
-          return <Champ name={champ.name}
-                  enemytips={champ.enemytips}/>
-        } )}
+          {this.state.champs.map( ( champ) => {
+            return <Champ name={champ.name}
+                    enemytips={champ.enemytips}/>
+          } )}
+
         </p>
       </div>
-
-
-
 
     );
   }
