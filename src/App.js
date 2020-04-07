31ace07189e22ab55a6a6915f9ae70a7830fca91
Quinn Krug <quinn.krug@gmail.com>
2020-04-07 08:13:45 -0600

    wipe out old files and code to make room for new structure

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import classes from './App.css';
+
+class App extends Component {
+
+  //STATE
+  state = {
+
+  }
+
+//FUNCTIONS (METHODS)
+
+
+
+// BEGIN RENDERING
+  render() {
+
+
+    return (
+      <div className={classes.App}>
+      <h1> Know Your Enemy </h1>
+        
+
+      </div>
+
+
+
+    );
+  }
+}
+
+export default App;
