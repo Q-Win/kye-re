5bb9e2a765e29491d2ebae21935b4e7e0fc29f63
Quinn Krug <quinn.krug@gmail.com>
2020-04-07 09:28:54 -0600

    set up Champ Selector files

--- src/containers/TeamBuilder/TeamBuilder.js
+++ src/containers/TeamBuilder/TeamBuilder.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux/Aux';
 import classes from './TeamBuilder.css'
+import ChampSelector from '../components/ChampSelector/ChampSelector'
 
 class TeamBuilder extends Component {
 
   render (){
     return(
     <Aux>
-      <h1>champ select</h1>
+      <form>
+       <fieldset>
+          <legend>Selecting elements</legend>
+          <p>
+             <label>Select list</label>
+             <select id = "myList">
+               <option value = "1">one</option>
+               <option value = "2">two</option>
+               <option value = "3">three</option>
+               <option value = "4">four</option>
+             </select>
+          </p>
+       </fieldset>
+    </form>
     </Aux>
 
   )}
