76b5d4183bd50a80ab5752fd7a001baa9a2f0c23
Quinn Krug <quinn.krug@gmail.com>
2020-04-08 14:36:47 -0600

    intial file setup for EnemyBuilder

--- src/containers/EnemyBuilder/EnemyBuilder.js
+++ src/containers/EnemyBuilder/EnemyBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 
+class EnemyBuilder extends Component {
 
   state ={
     enemyData: {
@@ -10,6 +11,40 @@ import React, { Component } from 'react';
         cooldownsW: null,
         cooldownsE: null,
         cooldownsR: null,
+      },
+      champ2: {
+        enemytips: null,
+        cooldownsQ: null,
+        cooldownsW: null,
+        cooldownsE: null,
+        cooldownsR: null,
+      },
+      champ3: {
+        enemytips: null,
+        cooldownsQ: null,
+        cooldownsW: null,
+        cooldownsE: null,
+        cooldownsR: null,
+      },
+      champ4: {
+        enemytips: null,
+        cooldownsQ: null,
+        cooldownsW: null,
+        cooldownsE: null,
+        cooldownsR: null,
+      },
+      champ5: {
+        enemytips: null,
+        cooldownsQ: null,
+        cooldownsW: null,
+        cooldownsE: null,
+        cooldownsR: null,
       }
     }
   }
+
+  render (){
+    return(  )}
+}
+
+export default EnemyBuilder;
