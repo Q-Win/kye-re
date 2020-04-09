3361da89523c1a3d9240b48b9773dcb12f270bd4
Quinn Krug <quinn.krug@gmail.com>
2020-04-09 14:03:23 -0600

    Modal populating with dummy data

--- src/containers/EnemyBuilder/EnemyBuilder.js
+++ src/containers/EnemyBuilder/EnemyBuilder.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Aux from '../../hoc/Aux/Aux';
+import Enemy from '../../components/Enemy/Enemy'
 
 
 class EnemyBuilder extends Component {
 
+  state ={
+    champ1: {
+      name: "Cat",
+      enemytips: "run",
+      cooldownQ: [],
+      cooldownW: [],
+      cooldownE: [],
+      cooldownR: [],
+    },
+    champ2: {
+      name: "",
+      enemytips: "",
+      cooldownQ: [],
+      cooldownW: [],
+      cooldownE: [],
+      cooldownR: [],
+    },
+    champ3: {
+      name: "",
+      enemytips: "",
+      cooldownQ: [],
+      cooldownW: [],
+      cooldownE: [],
+      cooldownR: [],
+    },
+    champ4: {
+      name: "",
+      enemytips: "",
+      cooldownQ: [],
+      cooldownW: [],
+      cooldownE: [],
+      cooldownR: [],
+    },
+    champ5: {
+      name: "",
+      enemytips: "",
+      cooldownQ: [],
+      cooldownW: [],
+      cooldownE: [],
+      cooldownR: [],
+    }
+  }
 
 
   render (){
     return(
       <Aux>
-        <div><h1>Enemies</h1></div>
+        <Enemy name={this.state.champ1.name} enemytips={this.state.champ1.enemytips} />
+        <Enemy name={this.state.champ1.name} enemytips={this.state.champ1.enemytips} />
+        <Enemy name={this.state.champ1.name} enemytips={this.state.champ1.enemytips} />
+        <Enemy name={this.state.champ1.name} enemytips={this.state.champ1.enemytips} />
+        <Enemy name={this.state.champ1.name} enemytips={this.state.champ1.enemytips} />
       </Aux>
       )}
 }
