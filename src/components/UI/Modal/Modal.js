9ff510f2d561c8baa0eb10af5b56ef4326c6d41b
Quinn Krug <quinn.krug@gmail.com>
2020-04-09 13:47:05 -0600

    add Aux back into Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,13 +7,14 @@ class Modal extends Component {
 
   render (){
     return (
+      <Aux>
         <div className={classes.Modal}
           style={{
           transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
           opacity: this.props.show ? '1': '0'}}>
           {this.props.children}
         </div>
-
+      </Aux>
   )}
 }
 
