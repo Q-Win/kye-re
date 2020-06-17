dd2cdb187c242b0b8ae1fed22d7f44cc191d14e6
Quinn Krug <quinn.krug@gmail.com>
2020-06-17 17:19:11 -0600

    create css file for skill Champion.js

--- src/components/Champion/Champion.js
+++ src/components/Champion/Champion.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import SkillTable from './SkillTable/SkillTable'
+import classes from './Champion.css';
 
 const champion = (props) => (
   <div>
     <h1>{props.champData.name}</h1>
     <p>{props.champData.enemytips}</p>
-    <SkillTable
-      cooldowns = {props.champData.cooldownQ}
-      description = {props.champData.descriptionQ}/>
-    <SkillTable
-      cooldowns = {props.champData.cooldownW}
-      description = {props.champData.descriptionW}/>
-    <SkillTable
-      cooldowns = {props.champData.cooldownE}
-      description = {props.champData.descriptionE}/>
-    <SkillTable
-      cooldowns = {props.champData.cooldownR}
-      description = {props.champData.descriptionR}/>
+    <div className={classes.Tables}>
+      <SkillTable
+        cooldowns = {props.champData.cooldownQ}
+        description = {props.champData.descriptionQ}/>
+      <SkillTable
+        cooldowns = {props.champData.cooldownW}
+        description = {props.champData.descriptionW}/>
+      <SkillTable
+        cooldowns = {props.champData.cooldownE}
+        description = {props.champData.descriptionE}/>
+      <SkillTable
+        cooldowns = {props.champData.cooldownR}
+        description = {props.champData.descriptionR}/>
+    </div>
   </div>
 );
 
